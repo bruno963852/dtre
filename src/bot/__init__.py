d506ae077dc4ddaf9ab0ceaa9d40f0f7add5d5e4
Bruno Martins <bruno963852@gmail.com>
2020-06-11 20:17:53 -0300

    added some logging and enhanced the error messages

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import sys
 import traceback
 from typing import Dict
 
@@ -32,31 +34,57 @@ async def on_ready():
     bot.add_cog(ScenarioCommands(bot))
     bot.add_cog(CharacterCommands(bot))
     bot.add_cog(DiceRollCommands(bot))
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    logging.info(f'Logged in as "{bot.user.name}" / {bot.user.id}')
 
 
-@bot.check
-async def check_permission(ctx: commands.Context):
-    if not ctx.channel.permissions_for(ctx.me).manage_messages:
-        await ctx.send('O bot precisa da permissão de "Gerenciar Mensagens" para apagar as mensagens do canal da mesa')
-        return False
-    # elif not ctx.channel.permissions_for(ctx.me).manage_channels:
-    #     await ctx.send('O bot precisa da permissão de "Gerenciar Canais" para criar o canal da mesa')
-    #     return False
-    # await ctx.message.delete()
-    return True
+# @bot.check
+# async def check_permission(ctx: commands.Context):
+#     if not ctx.channel.permissions_for(ctx.me).manage_messages:
+#         await ctx.send('O bot precisa da permissão de "Gerenciar Mensagens" para apagar as mensagens do canal da mesa')
+#         return False
+#     # elif not ctx.channel.permissions_for(ctx.me).manage_channels:
+#     #     await ctx.send('O bot precisa da permissão de "Gerenciar Canais" para criar o canal da mesa')
+#     #     return False
+#     # await ctx.message.delete()
+#     return True
 
 
-# @bot.event
-# async def on_command_error(ctx: commands.Context, error: commands.CommandError):
-#     print(error)
-#     if error is CheckFailure:
-#         await ctx.send("Erro, o bot não tem as permissões necessárias...")
-#     else:
-#         await ctx.send("Houve um erro inesperado...")
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    ignored = (commands.CommandNotFound,)
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, ignored):
+        pass
+    
+    elif isinstance(error, commands.UserInputError):
+        return await ctx.send(f'There was an **error** in the command arguments...')
+
+    elif isinstance(error, commands.DisabledCommand):
+        return await ctx.send(f'{ctx.command} has been disabled...')
+
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages...')
+        except:
+            pass
+
+    elif isinstance(error, commands.BadArgument):
+        if ctx.command.qualified_name == 'tag list':
+            return await ctx.send('I could not find that member. Please try again.')
+
+    await ctx.send('I rolled a **Critical Failure** executinng that command...')
+
+    logging.error('Ignoring exception in command {}:'.format(ctx.command))
+    logging.error('Stack info', stack_info=True)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 
 def run():
--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
 import src.bot as bot
+import logging
+import sys
+
+# noinspection PyArgumentList
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] -%(pathname)s- %(message)s",
+    handlers=[
+        logging.FileHandler('../debug.log'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logging.info("Initializing application...")
 
 bot.run()
